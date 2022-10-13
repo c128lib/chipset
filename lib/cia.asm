798eb5ea5b52f0ab4d357a238de5c9b0c967b300
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2022-10-13 22:22:44 +0200

    Added Cia and Vdc

---
+++ lib/cia.asm
@@ -0,0 +1,85 @@
+#importonce
+.filenamespace c128lib
+
+// CIA1
+.label CIA1               = $DC00 
+.label CIA1_DATA_PORT_A   = CIA1 + $00
+.label CIA1_DATA_PORT_B   = CIA1 + $01
+.label CIA1_DATA_DIR_A    = CIA1 + $02
+.label CIA1_DATA_DIR_B    = CIA1 + $03
+.label CIA1_TIMER_A_LO    = CIA1 + $04 
+.label CIA1_TIMER_A_HI    = CIA1 + $05 
+.label CIA1_TIMER_B_LO    = CIA1 + $06 
+.label CIA1_TIMER_B_HI    = CIA1 + $07 
+.label CIA1_TOD_SEC10     = CIA1 + $08 
+.label CIA1_TOD_SEC       = CIA1 + $09
+.label CIA1_TOD_MIN       = CIA1 + $0A
+.label CIA1_TOD_HOUR      = CIA1 + $0B
+.label CIA1_IO_BUFFER     = CIA1 + $0C
+.label CIA1_IRQ_CONTROL   = CIA1 + $0D
+.label CIA1_CONTROL_A     = CIA1 + $0E
+.label CIA1_CONTROL_B     = CIA1 + $0F
+
+// CIA2
+.label CIA2               = $DD00
+.label CIA2_DATA_PORT_A   = CIA2 + $00
+.label CIA2_DATA_PORT_B   = CIA2 + $01
+.label CIA2_DATA_DIR_A    = CIA2 + $02
+.label CIA2_DATA_DIR_B    = CIA2 + $03
+.label CIA2_TIMER_A_LO    = CIA2 + $04 
+.label CIA2_TIMER_A_HI    = CIA2 + $05 
+.label CIA2_TIMER_B_LO    = CIA2 + $06 
+.label CIA2_TIMER_B_HI    = CIA2 + $07 
+.label CIA2_TOD_SEC10     = CIA2 + $08 
+.label CIA2_TOD_SEC       = CIA2 + $09
+.label CIA2_TOD_MIN       = CIA2 + $0A
+.label CIA2_TOD_HOUR      = CIA2 + $0B
+.label CIA2_IO_BUFFER     = CIA2 + $0C
+.label CIA2_IRQ_CONTROL   = CIA2 + $0D
+.label CIA2_CONTROL_A     = CIA2 + $0E
+.label CIA2_CONTROL_B     = CIA2 + $0F
+
+// Joystick flags
+.label JOY_UP           = %00001
+.label JOY_DOWN         = %00010
+.label JOY_LEFT         = %00100
+.label JOY_RIGHT        = %01000
+.label JOY_FIRE         = %10000
+
+// VIC-II memory banks
+.label BANK_0           = %00000011
+.label BANK_1           = %00000010
+.label BANK_2           = %00000001
+.label BANK_3           = %00000000
+
+/*
+ * Configures memory "bank" (16K) which is directly addressable by VIC2 chip.
+ *
+ * MOD: A
+ */
+.macro setVICBank(bank) {
+  lda CIA2_DATA_PORT_A
+  and #%11111100
+  ora #[bank & %00000011]
+  sta CIA2_DATA_PORT_A
+}
+.assert "setVICBank(BANK_0) sets 11", { :setVICBank(BANK_0) }, {
+  lda $DD00
+  and #%11111100
+  ora #%00000011
+  sta $DD00
+}
+.assert "setVICBank(BANK_3) sets 00", { :setVICBank(BANK_3) }, {
+  lda $DD00
+  and #%11111100
+  ora #%00000000
+  sta $DD00
+}
+
+.macro disableCIAInterrupts() {
+  lda #$7F                     
+  sta CIA1_IRQ_CONTROL
+  sta CIA2_IRQ_CONTROL
+  lda CIA1_IRQ_CONTROL
+  lda CIA2_IRQ_CONTROL
+}
--- lib/mos8502.asm
+++ lib/mos8502.asm
@@ -8,6 +8,8 @@
 .label MOS_8502_DIRECTION       = $00
 .label MOS_8502_IO              = $01
 
+.label MMURCR 		              = $ff06 	// Ram configuration register
+
 /*
  * I/O Register bits.
  */
@@ -45,4 +47,61 @@
   nmi: 
     rti
   end:
+}
+
+/*----------------------------------------------------------
+Configure common RAM amount.
+
+RAM Bank 0 is always the visible RAM bank.
+Valid values are 1,4,8 and 16.
+
+Syntax:		:SetCommonRAM(1)
+----------------------------------------------------------*/
+.macro SetCommonRAM(amount) {
+	lda MMURCR
+	and #%11111100 			// clear bits 0 and 1. this is also option 1
+	.if(amount==4) {
+		ora #%00000001
+	}
+	.if(amount==8) {
+		ora #%00000010
+	}
+	.if(amount==16) {
+		ora #%00000011
+	}
+	sta MMURCR
+}
+
+/*----------------------------------------------------------
+Configure where common RAM is enabled. Top, bottom, or both.
+Valid options are 1, 2 or 3.
+1 = bottom (default)
+2 = top
+3 = bottom and top
+
+Syntax:		:SetCommonEnabled(1)
+----------------------------------------------------------*/
+.macro SetCommonEnabled(option) {
+	lda MMURCR
+	and #%11110011 			// clear bits 2 and 3
+	ora #option*4
+	sta MMURCR
+}
+
+/*----------------------------------------------------------
+ Set RAM block that the VIC chip will use, bit 6 of MMUCR.
+ Only useful for text display. Pretty useless, really.
+ Kernal routines use RAM0, so you need to roll your own routines.
+
+ Use SetVICBank() to set the 16k block that the VIC will use in that block.
+
+ Syntax:		:SetVICRamBank(0 or 1)
+ ----------------------------------------------------------*/
+.macro SetVICRAMBank(value) {
+	lda MMURCR
+	and #%10111111 			// clear bit 6
+	.if(value==1) {
+		ora #%01111111 		// enable bit 6
+	}
+	sta MMURCR
 }
