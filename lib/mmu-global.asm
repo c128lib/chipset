03d3d2df0a3162421c1478e54a9fd56bd9bbff1e
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2022-12-03 15:49:59 +0100

    Fix mmu labels and macro

--- lib/mmu-global.asm
+++ lib/mmu-global.asm
@@ -4,6 +4,7 @@
 
 .macro @c128lib_SetCommonRAM(config) { SetCommonRAM(config) }
 .macro @c128lib_SetMMUConfiguration(config) { SetMMUConfiguration(config) }
+.macro @c128lib_SetMMULoadConfiguration(config) { SetMMULoadConfiguration(config) }
 .macro @c128lib_SetBankConfiguration(config) { SetBankConfiguration(config) }
 .macro @c128lib_SetVICRAMBank(value) { SetVICRAMBank(value) }
 .macro @c128lib_SetIOBank(bank, bankname) { SetIOBank(bank, bankname) }
--- lib/mmu.asm
+++ lib/mmu.asm
@@ -14,28 +14,26 @@
 
 .namespace Mmu {
 
-.label CONFIGURATION        = $D500
+.label CONFIGURATION        = $D500   // Bank configuration register
 .label PRECONFIG_A          = $D501
 .label PRECONFIG_B          = $D502
 .label PRECONFIG_C          = $D503
 .label PRECONFIG_D          = $D504
-.label MODE_CONFIG          = $D505
-.label RAM_CONFIG           = $D506
+.label MODE_CONFIG          = $D505   // Cpu mode configuration register
+.label RAM_CONFIG           = $D506   // Ram configuration register
 .label PAGE0_PAGE_POINTER   = $D507
 .label PAGE0_BLOCK_POINTER  = $D508
 .label PAGE1_PAGE_POINTER   = $D509
 .label PAGE1_BLOCK_POINTER  = $D50A
 .label MMU_VERSION          = $D50B
 
-.label MMUCR                = $FF00   // bank configuration register
-.label LOAD_CONFIG_A        = $FF01
-.label LOAD_CONFIG_B        = $FF02
-.label LOAD_CONFIG_C        = $FF03
-.label LOAD_CONFIG_D        = $FF04
-.label MMUMCR               = $FF05   // cpu mode configuration register
-.label MMURCR               = $FF06   // ram configuration register
+.label LOAD_CONFIGURATION   = $FF00
+.label LOAD_PRECONFIG_A     = $FF01
+.label LOAD_PRECONFIG_B     = $FF02
+.label LOAD_PRECONFIG_C     = $FF03
+.label LOAD_PRECONFIG_D     = $FF04
 
-// MMUCR bit handling
+// CONFIGURATION bit handling
 // bit 0 - controls io active on $d000-$dfff
 .label IO_ROM             = %00000000
 .label IO_RAM             = %00000001
@@ -53,12 +51,37 @@
 // bit 4-5 - controls rom mid space $c000-$ffff (Screen editor rom, kernal rom)
 .label ROM_HI             = %00000000   // Screen editor ROM ($c000-$cfff), character ROM ($d000-$Ddfff), Kemal ROM ($e000-$ffff)
 .label ROM_HI_INT         = %00010000   // Internal function ROM: refers to ROM in the free ROM socket on the 128 circuit board.
-.label ROM_HI_EXT         = %00100000   // External function ROM: refers to ROM in a cartridge plugged into the expansion port. 
+.label ROM_HI_EXT         = %00100000   // External function ROM: refers to ROM in a cartridge plugged into the expansion port.
 .label ROM_HI_RAM         = %00110000
 
 // bit 6
 .label RAM0               = %00000000
 .label RAM1               = %01000000
+
+// MODE_CONFIG bit handling
+// bit 0 Cpu active
+.label CPU_Z80            = %00000000
+.label CPU_8502           = %00000001
+
+// bit 3 Fast serial input/output
+.label FASTSERIALINPUT    = %00000000
+.label FASTSERIALOUTPUT   = %00001000
+
+// bit 4 GAME
+.label GAME_LOW           = %00000000
+.label GAME_HI            = %00010000
+
+// bit 5 EXROM
+.label EXROM_LOW          = %00000000
+.label EXROM_HI           = %00100000
+
+// bit 6 Kernal 128/64
+.label KERNAL_128         = %00000000
+.label KERNAL_64          = %01000000
+
+// bit 7 40/80 cols
+.label COLS_80            = %00000000
+.label COLS_40            = %10000000
 
 // MMURCR bit handling
 // bit 0-1 Amount of common ram
@@ -80,13 +103,28 @@
 
   Refer to IO_*, ROM_*, RAM* label to generate input value
 
-  Syntax:    SetMMUConfiguration(RAM0 | ROM_HI | ROM_MID_RAM | ROM_LOW_ROM | IO_ROM)
+  Syntax: SetMMUConfiguration(Mmu.RAM0 | Mmu.ROM_HI | Mmu.ROM_MID_RAM | Mmu.ROM_LOW_ROM | Mmu.IO_ROM)
 */
 .macro SetMMUConfiguration(config) {
     lda #config
-    sta Mmu.MMUCR
+    sta Mmu.CONFIGURATION
 }
 .assert "SetMMUConfiguration(RAM1 | ROM_HI_RAM | ROM_MID_RAM | ROM_LOW_RAM | IO_RAM) sets accumulator to 7f", { SetMMUConfiguration(Mmu.RAM1 | Mmu.ROM_HI_RAM | Mmu.ROM_MID_RAM | Mmu.ROM_LOW_RAM | Mmu.IO_RAM) }, {
+  lda #%01111111; sta $d500
+}
+
+/*
+  Banking, RAM configurations. Uses $FF00 instead of $D500.
+
+  Refer to IO_*, ROM_*, RAM* label to generate input value
+
+  Syntax: SetMMULoadConfiguration(Mmu.RAM0 | Mmu.ROM_HI | Mmu.ROM_MID_RAM | Mmu.ROM_LOW_ROM | Mmu.IO_ROM)
+*/
+.macro SetMMULoadConfiguration(config) {
+    lda #config
+    sta Mmu.LOAD_CONFIGURATION
+}
+.assert "SetMMULoadConfiguration(RAM1 | ROM_HI_RAM | ROM_MID_RAM | ROM_LOW_RAM | IO_RAM) sets accumulator to 7f", { SetMMULoadConfiguration(Mmu.RAM1 | Mmu.ROM_HI_RAM | Mmu.ROM_MID_RAM | Mmu.ROM_LOW_RAM | Mmu.IO_RAM) }, {
   lda #%01111111; sta $ff00
 }
 
@@ -103,7 +141,7 @@
   Setting a bit means RAM, clearing means ROM.
   Use the BASIC Bank configuration numbers.
 
-  Syntax:    SetBankConfiguration(number)
+  Syntax: SetBankConfiguration(number)
 */
 .macro SetBankConfiguration(id) {
     .if(id==0) {
@@ -124,7 +162,22 @@
     .if(id==99) {
       lda #%00001110  // IO, kernal, RAM0. No basic,48K RAM.
     }
-    sta Mmu.MMUCR
+    sta Mmu.LOAD_CONFIGURATION
+}
+
+/*
+  Set mode configuration register.
+
+  Syntax: SetModeConfig(Mmu.CPU_8502 | Mmu.KERNAL_C64)
+*/
+.macro SetModeConfig(config) {
+    lda #config
+    sta Mmu.MODE_CONFIG
+}
+.assert "SetModeConfig(CPU_8502 | FASTSERIALOUTPUT | GAME_HI | EXROM_HI | KERNAL_64 | COLS_40) sets accumulator to 0f", {
+    SetModeConfig(Mmu.CPU_8502 | Mmu.FASTSERIALOUTPUT | Mmu.GAME_HI | Mmu.EXROM_HI | Mmu.KERNAL_64 | Mmu.COLS_40)
+}, {
+  lda #%11111001; sta $d505
 }
 
 /*
@@ -135,14 +188,14 @@
   For ex. if you choose 4K common ram at top and bottom
   you'll have 4K up and 4K bottom.
 
-  Syntax:    SetCommonRAM(COMMON_RAM_4K | COMMON_RAM_BOTH)
+  Syntax:    SetCommonRAM(Mmu.COMMON_RAM_4K | Mmu.COMMON_RAM_BOTH)
 */
 .macro SetCommonRAM(config) {
     lda #config
-    sta Mmu.MMURCR
+    sta Mmu.RAM_CONFIG
 }
 .assert "SetCommonRAM(COMMON_RAM_16K | COMMON_RAM_BOTH) sets accumulator to 0f", { SetCommonRAM(Mmu.COMMON_RAM_16K | Mmu.COMMON_RAM_BOTH) }, {
-  lda #%00001111; sta $ff06
+  lda #%00001111; sta $d506
 }
 
 /*
@@ -155,18 +208,18 @@
   Syntax:    SetVICRamBank(0 or 1)
 */
 .macro SetVICRAMBank(value) {
-    lda Mmu.MMURCR
+    lda Mmu.RAM_CONFIG
     and #%10111111       // clear bit 6
     .if(value==1) {
       ora #%01111111     // enable bit 6
     }
-    sta Mmu.MMURCR
+    sta Mmu.RAM_CONFIG
 }
 .assert "SetVICRAMBank(0) sets accumulator to 0f", { SetVICRAMBank(0) }, {
-  lda $ff06;and #%10111111;sta $ff06
+  lda $d506;and #%10111111;sta $d506
 }
 .assert "SetVICRAMBank(1) sets accumulator to 0f", { SetVICRAMBank(1) }, {
-  lda $ff06;and #%10111111;ora #%01111111;sta $ff06
+  lda $d506;and #%10111111;ora #%01111111;sta $d506
 }
 
 /*
