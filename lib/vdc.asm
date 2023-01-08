6cdc8680483059ce320fb6ac06653aea4051b59c
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2023-01-08 17:03:01 +0100

    Renaming WriteToVdcMemory to WriteToVdcMemoryByCoordinates

--- lib/vdc.asm
+++ lib/vdc.asm
@@ -190,7 +190,7 @@
     stx $d600; bit $d600; bpl *-3; sta $d601
 }
 
-.macro WriteToVdcMemory(source, xPos, yPos) {
+.macro WriteToVdcMemoryByCoordinates(source, xPos, yPos) {
   .errorif (xPos == -1 && yPos != -1), "xPos and yPos must be -1 at same time"
   .errorif (xPos != -1 && yPos == -1), "xPos and yPos must be -1 at same time"
   .errorif (xPos < -1 || yPos < -1), "xPos and yPos can't be lower than -1"
@@ -209,16 +209,16 @@
     iny
     bne CopyLoop
 }
-.asserterror "WriteToVdcMemory($beef, -1, 0)", { WriteToVdcMemory($beef, -1, 0) }
-.asserterror "WriteToVdcMemory($beef, 0, -1)", { WriteToVdcMemory($beef, 0, -1) }
-.asserterror "WriteToVdcMemory($beef, -2, 0)", { WriteToVdcMemory($beef, -2, 0) }
-.asserterror "WriteToVdcMemory($beef, 0, -2)", { WriteToVdcMemory($beef, 0, -2) }
-.asserterror "WriteToVdcMemory($beef, -2, -2)", { WriteToVdcMemory($beef, -2, -2) }
-.assert "WriteToVdcMemory($beef, -1, -1)", { WriteToVdcMemory($beef, -1, -1) },
+.asserterror "WriteToVdcMemoryByCoordinates($beef, -1, 0)", { WriteToVdcMemoryByCoordinates($beef, -1, 0) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, 0, -1)", { WriteToVdcMemoryByCoordinates($beef, 0, -1) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, -2, 0)", { WriteToVdcMemoryByCoordinates($beef, -2, 0) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, 0, -2)", { WriteToVdcMemoryByCoordinates($beef, 0, -2) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, -2, -2)", { WriteToVdcMemoryByCoordinates($beef, -2, -2) }
+.assert "WriteToVdcMemoryByCoordinates($beef, -1, -1)", { WriteToVdcMemoryByCoordinates($beef, -1, -1) },
 {
     ldy #0; lda $beef, y; jsr $CDCA; iny; bne *-7;
 }
-.assert "WriteToVdcMemory($beef, 1, 1)", { WriteToVdcMemory($beef, 1, 1) },
+.assert "WriteToVdcMemoryByCoordinates($beef, 1, 1)", { WriteToVdcMemoryByCoordinates($beef, 1, 1) },
 {
     ldx #$12; lda #0; jsr $CDCC; lda #81; inx; jsr $CDCC; ldy #0
     lda $beef, y; jsr $CDCA; iny; bne *-7;
