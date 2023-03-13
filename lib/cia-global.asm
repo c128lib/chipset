040ff18a317328403ea3872d3ea03fe15de510bd
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2023-03-13 20:51:02 +0100

    Added support for Return keypress/release

--- lib/cia-global.asm
+++ lib/cia-global.asm
@@ -8,6 +8,10 @@
 .filenamespace c128lib
 
 .macro @c128lib_SetVICBank(bank) { SetVICBank(bank) }
+
+.macro @c128lib_IsReturnPressed() { IsReturnPressed() }
+.macro @c128lib_IsReturnPressedAndReleased() { IsReturnPressedAndReleased() }
+
 .macro @c128lib_GetFirePressedPort1() { GetFirePressedPort1() }
 .macro @c128lib_GetFirePressedPort2() { GetFirePressedPort2() }
 .macro @c128lib_disableCIAInterrupts() { disableCIAInterrupts() }
--- lib/cia.asm
+++ lib/cia.asm
@@ -89,6 +89,63 @@
   sta $DD00
 }
 
+#if DETECTKEYPRESSED
+DetectKeyPressed: {
+    sei
+    lda #%11111111
+    sta c128lib.Cia.CIA1_DATA_DIR_A
+    lda #%00000000
+    sta c128lib.Cia.CIA1_DATA_DIR_B
+
+    lda MaskOnPortA
+    sta c128lib.Cia.CIA1_DATA_PORT_A
+    lda c128lib.Cia.CIA1_DATA_PORT_B
+    and MaskOnPortB
+    beq Pressed
+    lda #$00
+    jmp !+
+  Pressed:
+    lda #$01
+  !:
+    cli
+    rts
+
+  MaskOnPortA:    .byte $00
+  MaskOnPortB:    .byte $00
+}
+#endif
+
+.macro IsReturnPressed() {
+  #if (!DETECTKEYPRESSED)
+    .error "You should use #define DETECTKEYPRESSED"
+  #else
+    lda #%11111110
+    sta DetectKeyPressed.MaskOnPortA
+    lda #%00000010
+    sta DetectKeyPressed.MaskOnPortB
+  !:
+    jsr DetectKeyPressed
+    beq !-
+  #endif
+}
+
+.macro IsReturnPressedAndReleased() {
+  #if (!DETECTKEYPRESSED)
+    .error "You should use #define DETECTKEYPRESSED"
+  #else
+    lda #%11111110
+    sta DetectKeyPressed.MaskOnPortA
+    lda #%00000010
+    sta DetectKeyPressed.MaskOnPortB
+  !:
+    jsr DetectKeyPressed
+    beq !-
+  !:
+    jsr DetectKeyPressed
+    bne !-
+  #endif
+}
+
 /*
   Check if joystick port 1 fire button is pressed.
 
